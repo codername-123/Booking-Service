1a2dd58c6e01d8b2b57c31ce1c28c2e5daca80d9
codername-132 <nitin.rawat629@gmail.com>
2023-06-26 16:37:09 +0530

    fix typo

--- src/migrations/20230528061853-create-booking.js
+++ src/migrations/20230528061853-create-booking.js
@@ -11,7 +11,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      flightid: {
+      flightId: {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
