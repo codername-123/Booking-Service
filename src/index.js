5fc0aef8732fd5dd274305acabd4001432eab718
codername-132 <nitin.rawat629@gmail.com>
2023-06-26 16:39:49 +0530

    connect to queue and call cron jobs when starting the booking service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const crons = require("./utils/common/cron-jobs");
+const { Queue } = require("./config");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Server started listening on ${ServerConfig.PORT}`);
+  crons();
+  await Queue.connectQueue();
 });
